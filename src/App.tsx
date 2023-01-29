eca9849990fe23f4e4b6d66d2749030d1c18a9b8
George Zalokostas <gzalos6@gmail.com>
2023-01-29 20:23:51 +0200

    Fetching movies from API

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,23 @@
-import React, { useState } from "react";
-import MovieList from './components/MovieList';
+import React, { useState, useEffect } from "react";
+import { isTemplateExpression } from "typescript";
+import MovieList from "./components/MovieList";
 
 const App = () => {
-  const [movies, setMovies] = useState([
-    {
-      Title: "The Lord of the Rings: The Fellowship of the Ring",
-      Year: "2001",
-      imdbID: "tt0120737",
-      Type: "movie",
-      Poster:
-        "https://m.media-amazon.com/images/M/MV5BN2EyZjM3NzUtNWUzMi00MTgxLWI0NTctMzY4M2VlOTdjZWRiXkEyXkFqcGdeQXVyNDUzOTQ5MjY@._V1_SX300.jpg",
-    },
-    {
-      Title: "The Lord of the Rings: The Return of the King",
-      Year: "2003",
-      imdbID: "tt0167260",
-      Type: "movie",
-      Poster:
-        "https://m.media-amazon.com/images/M/MV5BNzA5ZDNlZWMtM2NhNS00NDJjLTk4NDItYTRmY2EwMWZlMTY3XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg",
-    },
-    {
-      Title: "The Lord of the Rings: The Two Towers",
-      Year: "2002",
-      imdbID: "tt0167261",
-      Type: "movie",
-      Poster:
-        "https://m.media-amazon.com/images/M/MV5BZGMxZTdjZmYtMmE2Ni00ZTdkLWI5NTgtNjlmMjBiNzU2MmI5XkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg",
-    },
-  ]);
+  const [movies, setMovies] = useState([]);
+
+  const getMovieRequest = async () => {
+    const url = "http://www.omdbapi.com/?apikey=19260989&s=Shawshank";
+
+    const response = await fetch(url);
+    const responseJson = await response.json();
+
+    setMovies(responseJson.Search);
+  };
+
+  useEffect(() => {
+    getMovieRequest();
+  }, []);
+
   return (
     <div>
       <MovieList movies={movies} />
