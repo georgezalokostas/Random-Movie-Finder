d9efb99a7e56f60b3d4387e78132ebfcb668650b
George Zalokostas <gzalos6@gmail.com>
2023-02-11 15:49:38 +0200

    Hide searchbox after run

--- src/components/SearchButton.tsx
+++ src/components/SearchButton.tsx
@@ -12,26 +12,29 @@ const SearchButton = ({
   firstResult: IMovie;
 }) => {
   let searchTerm: string = "";
-  const [hideButton, setHideButton] = useState(false);
+  const [hideElements, setHideElements] = useState(false);
 
+  //TODO: Remove searchbox and get results from a list of IMDB tags
   return (
     <div className="searchbox">
-      <input
-        type="text"
-        placeholder="Search..."
-        onChange={(event) => {
-          searchTerm = event.target.value;
-        }}
-      />
-      {!hideButton && (
-        <button
-          onClick={() => {
-            getMovieRequest(searchTerm);
-            setHideButton(true);
-          }}
-        >
-          Click me
-        </button>
+      {!hideElements && (
+        <div className="input-button">
+          <input
+            type="text"
+            placeholder="Search..."
+            onChange={(event) => {
+              searchTerm = event.target.value;
+            }}
+          />
+          <button
+            onClick={() => {
+              getMovieRequest(searchTerm);
+              setHideElements(true);
+            }}
+          >
+            Click me
+          </button>
+        </div>
       )}
       <DisplayMovieDetails movie={firstResult} />
     </div>
