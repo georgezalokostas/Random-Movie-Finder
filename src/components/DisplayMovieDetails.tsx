e49380cf60641bd0d9bd0575154c0cb764410c2c
George Zalokostas <gzalos6@gmail.com>
2023-02-17 00:04:04 +0200

    Comment out

--- src/components/DisplayMovieDetails.tsx
+++ src/components/DisplayMovieDetails.tsx
@@ -15,10 +15,10 @@ const DisplayMovieDetails = ({ movie }: { movie: IMovie | undefined }) => {
         }
       </h3>
       <img src={movie.Poster} alt="movie"></img>
-      <h3>Title: {movie.Title}</h3>
-      <h3>Type: {movie.Type}</h3>
-      <h3>Year: {movie.Year}</h3>
-      <h3>ImdbID: {movie.imdbID}</h3>
+      <h3> {movie.Title + movie.Year}</h3>
+      {/* <h3>Year: {movie.Year}</h3> */}
+      {/* <h3>Type: {movie.Type}</h3> */}
+      {/* <h3>ImdbID: {movie.imdbID}</h3> */}
     </div>
 
     //TODO: Open movie in IMDB (redirect)
