80d349f59326c44f40c861da584b6a21c8820ad2
George Zalokostas <gzalos6@gmail.com>
2023-02-18 19:30:51 +0200

    Changes to interface

--- src/interfaces/IMovie.tsx
+++ src/interfaces/IMovie.tsx
@@ -1,7 +1,21 @@
 export interface IMovie {
   Title: string;
   Year: string;
+  Rated: string;
+  Released: string;
+  Runtime: string;
+  Genre: string;
+  Director: string;
+  Writer: string;
+  Actors: string;
+  Plot: string;
+  Language: string;
+  Country: string;
+  Awards: string;
+  Poster: string;
+  Metascore : string;
+  imdbRating: string;
+  imdbVotes: string;
   imdbID: string;
   Type: string;
-  Poster: string;
 }
