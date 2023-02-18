6dcb1e3b05b83c22abe23d9882bd9b5657e73a05
George Zalokostas <gzalos6@gmail.com>
2023-02-18 20:54:37 +0200

    Finetuning

--- src/components/DisplayMovieDetails.tsx
+++ src/components/DisplayMovieDetails.tsx
@@ -26,11 +26,16 @@ const DisplayMovieDetails = ({
         }
       </h3>
       <img src={movie.Poster} alt="Movie poster" />
-      <h3> {movie.Title + " (" + movie.Year + ")"} </h3>
-      <h3>{"⭐" + movie.imdbRating + "/10"}</h3>
-      <h5>{movie.Plot}</h5>
-      <h5>Directors: {movie.Director}</h5>
+      <h3>{movie.Title + " (" + movie.Year + ")"}</h3>
+      <h3>
+        <a href={"https://www.imdb.com/title/" + movie.imdbID} target="_blank">
+          Open in IMDB
+        </a>
+        {" - ⭐" + movie.imdbRating + "/10"}
+      </h3>
+      <h5>By: {movie.Director}</h5>
       <h5>Starring: {movie.Actors}</h5>
+      <h5>{movie.Plot}</h5>
       <br />
       <button
         className="search-button"
@@ -38,7 +43,7 @@ const DisplayMovieDetails = ({
           getMovieRequest(randomItemFromList);
         }}
       >
-       Search Next
+        Search Next
       </button>
     </div>
   );
