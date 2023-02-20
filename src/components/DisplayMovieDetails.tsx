58a2048d066ebd0cfacc30c093c62f03f2a56a12
George Zalokostas <gzalos6@gmail.com>
2023-02-20 17:55:20 +0200

    WIP: Creating css flexbox

--- src/components/DisplayMovieDetails.tsx
+++ src/components/DisplayMovieDetails.tsx
@@ -2,6 +2,7 @@ import { IMovie } from "../interfaces/IMovie";
 import { resultsHeaderText } from "./text_arrays/ResultsHeaderText";
 import { Top250MoviesIDs } from "./text_arrays/Top250MoviesIDs";
 import { getRandomItem } from "./Functions";
+import "./Styles.css";
 
 const DisplayMovieDetails = ({
   getMovieRequest,
@@ -17,28 +18,37 @@ const DisplayMovieDetails = ({
   const randomItemFromList = getRandomItem(Top250MoviesIDs);
 
   return (
-    <div className="details">
-      <h3>
-        {
-          resultsHeaderText[
-            Math.floor(Math.random() * resultsHeaderText.length)
-          ]
-        }
-      </h3>
-      <img src={movie.Poster} alt="Movie poster" />
-      <h3>{movie.Title + " (" + movie.Year + ")"}</h3>
-      <h3>
-        <a href={"https://www.imdb.com/title/" + movie.imdbID} target="_blank">
-          Open in IMDB
-        </a>
-        {" - ⭐" + movie.imdbRating + "/10"}
-      </h3>
-      <h5>By: {movie.Director}</h5>
-      <h5>Starring: {movie.Actors}</h5>
-      <h5>{movie.Plot}</h5>
-      <br />
+    <div>
+      <div className="results">
+        <h3 className="header">
+          {
+            resultsHeaderText[
+              Math.floor(Math.random() * resultsHeaderText.length)
+            ]
+          }
+        </h3>
+        <div className="poster">
+          <img src={movie.Poster} alt="Movie poster" />
+        </div>
+        <div className="details">
+          <h3>{movie.Title + " (" + movie.Year + ")"}</h3>
+          <h3>
+            <a
+              href={"https://www.imdb.com/title/" + movie.imdbID}
+              target="_blank"
+            >
+              Open in IMDB
+            </a>
+            {" - ⭐" + movie.imdbRating + "/10"}
+          </h3>
+          <h5>By: {movie.Director}</h5>
+          <h5>Starring: {movie.Actors}</h5>
+          <h5>{movie.Plot}</h5>
+          <br />
+        </div>
+      </div>
       <button
-        className="search-button"
+        className="nextMovieBtn"
         onClick={() => {
           getMovieRequest(randomItemFromList);
         }}
