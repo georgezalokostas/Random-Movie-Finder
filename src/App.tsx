bb6870e65028c22be2694865b7d2ae696410307d
George Zalokostas <gzalos6@gmail.com>
2023-03-08 21:54:54 +0200

    Simplified App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,21 +5,7 @@ import { Top250MoviesIDs } from "./components/text_arrays/Top250MoviesIDs";
 import { getRandomItem } from "./components/Functions";
 
 const App = () => {
-  const [movies, setMovies] = useState<IMovie[]>([]);
-  const [populateMovies, setPopulateMovies] = useState<boolean>(true);
-
-  useEffect(() => {
-    const PopulateMoviesArray = async () => {
-      await GetMovieData();
-    };
-
-    if (populateMovies === true) {
-      PopulateMoviesArray();
-      setPopulateMovies(false);
-    }
-  }, [populateMovies]);
-
-  const RenderMovie = () => movies.shift();
+  const [movie, setMovie] = useState<IMovie>();
 
   const GetMovieData = async () => {
     let searchTerm = getRandomItem(Top250MoviesIDs);
@@ -29,32 +15,27 @@ const App = () => {
     const responseJson = await response.json();
 
     if (responseJson) {
-      setMovies((oldData) => [...oldData, responseJson]);
-    } else {
-      console.log("Error: " + responseJson.Error);
+      setMovie(responseJson);
     }
   };
 
-  if (movies.length === 0) {
-    return <div>Loading...</div>;
-  } else {
-    return (
-      <div className="Components">
-        <RenderResults movie={RenderMovie()} />
-        <button
-          className="nextMovie"
-          onClick={() => {
-            if (movies.length < 3) {
-              setPopulateMovies(true);
-            }
-            <RenderResults movie={RenderMovie()} />;
-          }}
-        >
-          Next Movie!
-        </button>
-      </div>
-    );
-  }
+  useEffect(() => {
+    GetMovieData();  
+  },[]);
+
+  return (
+    <div className="Components">
+      <RenderResults movie={movie} />
+      <button
+        className="nextMovie"
+        onClick={() => {
+          GetMovieData();
+        }}
+      >
+        Next Movie!
+      </button>
+    </div>
+  );
 };
 
 export default App;
